c5853182412bb3b706254bffbb7db114d42066fc
liuxiangwin <alanliuxiang@hotmail.com>
2016-03-19 01:28:41 +0800

    TutoProductValueProvider

---
+++ consulting/consultingcore/src/com/hybris/core/search/solrfacetsearch/provider/impl/TutoProductContentValueProvider.java
@@ -0,0 +1,82 @@
+/**
+ *
+ */
+package com.hybris.core.search.solrfacetsearch.provider.impl;
+
+
+import de.hybris.platform.solrfacetsearch.config.IndexConfig;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.config.exceptions.FieldValueProviderException;
+import de.hybris.platform.solrfacetsearch.provider.FieldNameProvider;
+import de.hybris.platform.solrfacetsearch.provider.FieldValue;
+import de.hybris.platform.solrfacetsearch.provider.FieldValueProvider;
+import de.hybris.platform.solrfacetsearch.provider.impl.AbstractPropertyFieldValueProvider;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import com.hybris.core.model.ConsultantModel;
+import com.hybris.core.services.ConsultantService;
+
+
+/**
+ * @author Alan Liu
+ *
+ */
+public class TutoProductContentValueProvider extends AbstractPropertyFieldValueProvider implements FieldValueProvider,
+		Serializable
+{
+	private FieldNameProvider fieldNameProvider;
+
+
+	private ConsultantService consultantService;
+
+	@Override
+	public Collection<FieldValue> getFieldValues(final IndexConfig indexConfig, final IndexedProperty indexedProperty,
+			final Object model) throws FieldValueProviderException
+	{
+		final Collection<FieldValue> fieldValues = new ArrayList<FieldValue>();
+		final List<ConsultantModel> foundContentList = consultantService.getTutoContentProduct();
+		for (final ConsultantModel consultantModel : foundContentList)
+		{
+			if (consultantModel != null)
+			{
+				fieldValues.add(new FieldValue("Tuto-Content", consultantModel.getContent()));
+			}
+		}
+
+		return fieldValues;
+	}
+
+	protected FieldNameProvider getFieldNameProvider()
+	{
+		return fieldNameProvider;
+	}
+
+	@Required
+	public void setFieldNameProvider(final FieldNameProvider fieldNameProvider)
+	{
+		this.fieldNameProvider = fieldNameProvider;
+	}
+
+	/**
+	 * @return the consultantService
+	 */
+	public ConsultantService getConsultantService()
+	{
+		return consultantService;
+	}
+
+	/**
+	 * @param consultantService
+	 *           the consultantService to set
+	 */
+	public void setConsultantService(final ConsultantService consultantService)
+	{
+		this.consultantService = consultantService;
+	}
+}
