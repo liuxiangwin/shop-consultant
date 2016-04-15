101cd7a8fda144b8f5614674f5492e73632b777d
liuxiangwin <alanliuxiang@hotmail.com>
2016-04-15 20:42:30 +0800

    Fix the bug with SiteUtil java

--- consulting/consultingcore/src/com/hybris/core/util/SiteUtil.java
+++ consulting/consultingcore/src/com/hybris/core/util/SiteUtil.java
@@ -14,9 +14,9 @@ public class SiteUtil
 	public static final String SITE_UK = "uk";
 	public static final String SITE_ZH = "zh";
 	public static final int START_INDEX = 0;
-	public static final int END_INDEX = 1;
+	public static final int END_INDEX = 2;
 
-	public static final String UK_CUR = "GB";
-	public static final String ZH_CUR = "CN";
+	public static final String UK_REGION = "UK";
+	public static final String ZH_REGION = "CN";
 
 }
