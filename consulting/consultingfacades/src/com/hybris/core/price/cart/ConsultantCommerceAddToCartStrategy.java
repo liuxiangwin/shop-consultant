cedf2c516285ffde89a9b750cfff22da867dc872
liuxiangwin <alanliuxiang@hotmail.com>
2016-04-21 18:25:49 +0800

    ======================================== Update the ConsultantCommerceAddToCartSrategy
    ========================================

---
+++ consulting/consultingfacades/src/com/hybris/core/price/cart/ConsultantCommerceAddToCartStrategy.java
@@ -0,0 +1,45 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *
+ */
+package com.hybris.core.price.cart;
+
+import static de.hybris.platform.servicelayer.util.ServicesUtil.validateParameterNotNull;
+
+import de.hybris.platform.commerceservices.order.CommerceCartModificationException;
+import de.hybris.platform.commerceservices.order.impl.DefaultCommerceAddToCartStrategy;
+import de.hybris.platform.commerceservices.service.data.CommerceCartParameter;
+import de.hybris.platform.core.model.order.CartModel;
+import de.hybris.platform.core.model.product.ProductModel;
+
+
+/**
+ * Abstract strategy for adding items to the cart
+ */
+public class ConsultantCommerceAddToCartStrategy extends DefaultCommerceAddToCartStrategy
+
+{
+	@Override
+	public void validateAddToCart(final CommerceCartParameter parameters) throws CommerceCartModificationException
+	{
+		final CartModel cartModel = parameters.getCart();
+		final ProductModel productModel = parameters.getProduct();
+
+		validateParameterNotNull(cartModel, "Cart model cannot be null");
+		validateParameterNotNull(productModel, "Product model cannot be null");
+
+		if (parameters.getQuantity() < 1)
+		{
+			throw new CommerceCartModificationException("Quantity must not be less than one");
+		}
+	}
+}
