ce1af056bf3eb1d494298e06d0330e985804b9cb
liuxiangwin <alanliuxiang@hotmail.com>
2016-04-25 23:11:01 +0800

    =================================== Update the consultaing choosepage controller =====================================

--- consulting/consultingstorefront/web/src/com/hybris/storefront/countryselector/web/interceptors/beforeview/ChooseCountryController.java
+++ consulting/consultingstorefront/web/src/com/hybris/storefront/countryselector/web/interceptors/beforeview/ChooseCountryController.java
@@ -62,7 +62,6 @@ public class ChooseCountryController extends AbstractPageController
 	@Resource(name = "cmsPageService")
 	private CMSPageService cmsPageService;
 
-
 	@Autowired
 	private CountrySelectorStrategy countrySelectorStrategy;
 
@@ -101,26 +100,36 @@ public class ChooseCountryController extends AbstractPageController
 		{
 			if (selectCountry.equalsIgnoreCase("zh"))
 			{
-
-				return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/开发/c/Development";
-
 				//return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/Development/c/Development";
+				return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/开发/c/Development";
 			}
 			else
 			{
+				//return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/uk-consultingsite/en/Development/c/Development";
 				return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/uk-consultingsite/en/Development/c/Development";
 			}
 		}
-
-
-
 	}
+
+
+	@RequestMapping(value = "/changeCountry", method = RequestMethod.GET)
+	public String changeCountryFromLink(final HttpServletRequest request, final Model model, final HttpServletResponse response)
+			throws IOException, CMSItemNotFoundException
+	{
+		//The user purpose to open the choose page for another choice
+		final ContentPageModel contentPageModel = cmsPageService.getPageByLabel("chooseCountryPage");
+		model.addAttribute("chooseUrl", "/main/chooseCountry");
+		storeCmsPageInModel(model, contentPageModel);
+		setUpMetaDataForContentPage(model, contentPageModel);
+		updatePageTitle(model, contentPageModel);
+		return getViewForPage(model);
+	}
+
 
 	@RequestMapping(value = "/chooseCountry", method = RequestMethod.POST)
 	public String chooseCountry(@RequestParam(value = "country", required = true) final String country, final Model model,
 			final HttpServletRequest request, final HttpServletResponse response) throws CMSItemNotFoundException
 	{
-
 		final String cookiValue = country;
 		cookieGenerator.addCookie(response, cookiValue);
 		CookieUtils.addCookie(response, 360000, "country-selected", country);
@@ -128,19 +137,14 @@ public class ChooseCountryController extends AbstractPageController
 
 		if (country.equalsIgnoreCase("zh"))
 		{
-			//return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/Development/c/Development";
 			return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/开发/c/Development";
+			//return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/zh-consultingsite/zh/Development/c/Development";
 		}
 		else
 		{
 			return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/uk-consultingsite/en/Development/c/Development";
+			//return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/uk-consultingsite/en/Development/c/Development";
 		}
-		//There is no country in session memory
-		//model.addAttribute("sites", countrySelectorStrategy.getAllCMMSite());
-		//storeCmsPageInModel(model, getContentPageForLabelOrId(null));
-		//setUpMetaDataForContentPage(model, getContentPageForLabelOrId(null));
-		//updatePageTitle(model, getContentPageForLabelOrId(null));
-		//return "pages/choosecounty/chooseCountry";
 	}
 
 	protected void updatePageTitle(final Model model, final AbstractPageModel cmsPage)
@@ -154,9 +158,7 @@ public class ChooseCountryController extends AbstractPageController
 		String cmsSite = "";
 		if (sessionService.getCurrentSession().getAttribute(CountrySelectorStrategy.SESSION_SELECT_COUNTYR) != null)
 		{
-
 			cmsSite = sessionService.getCurrentSession().getAttribute(CountrySelectorStrategy.SESSION_SELECT_COUNTYR);
-
 			return REDIRECT_PREFIX + "https://localhost:9002/consultingstorefront/" + cmsSite + "?clear=true&site=" + "cmsSite";
 		}
 		else
